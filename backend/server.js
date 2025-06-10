09b7e77089636ae098cd67d6a1c5a6e80eb2fa0e
jessieakdogan <jessieakdogan@gmail.com>
2025-06-10 14:28:16 +0100

    Add cleanup route and GitHub Actions scheduler

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/run-cleanup', (req, res) => {
-  require('./utils/fileCleaner');
+  require('./cleanup');
   res.send('Cleanup started.');
 });
 
